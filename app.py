437f552120f66afb9ab76f17f524fe4efb05991a
1i5oj1oijre <98590004+1i5oj1oijre@users.noreply.github.com>
2023-06-08 22:57:43 +0100

    Design improved a bit.

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_form():
-    return render_template('result.html')
+    return render_template('index.html', data="")
 
 @app.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['text']
     processed_text = analyse_postcode(text)
     if processed_text:
-        return processed_text
+        return render_template('index.html', data=processed_text)
     else:
         return "No data found, please try again."
 
